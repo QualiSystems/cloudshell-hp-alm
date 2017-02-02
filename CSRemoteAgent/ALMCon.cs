87fe2797ebbc220bb2e3b02bbd4279583046fdf2
Moti C <moti.c@ticomsoft.com>
2017-02-02 15:22:43 +0200

    Moti add

--- CSRemoteAgent/ALMCon.cs
+++ CSRemoteAgent/ALMCon.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using TDAPIOLELib;
+using System.Threading;
+using System.Windows.Forms;
 
 namespace CSRAgent
 {
     class ALMCon
     {
         public TDConnection conn;
-        private Dictionary<string, string> Params;
+        static private Dictionary<string, string> Params;
+        public CStatus mStatus;
         public ALMCon()
         {
             Params = new Dictionary<string,string>();
@@ -21,8 +24,8 @@ namespace CSRAgent
                 conn = new TDConnectionClass();
             if (!conn.Connected)
             {
-                conn.InitConnectionEx(this.getValue("TDAPI_host_name"));
-                conn.ConnectProjectEx(this.getValue("domain_name"), this.getValue("project_name"), this.getValue("user_name"), this.getValue("password"));
+                conn.InitConnectionEx(this.GetValue("TDAPI_host_name"));
+                conn.ConnectProjectEx(this.GetValue("domain_name"), this.GetValue("project_name"), this.GetValue("user_name"), this.GetValue("password"));
             }
             return 0;
         }
@@ -33,33 +36,97 @@ namespace CSRAgent
             return 0;
         }
 
-        public string getValue(string prm_name)
+        public string GetValue(string prmName)
         {
-            if (Params.ContainsKey(prm_name))
+            if (Params.ContainsKey(prmName))
             {
-                return  Params[prm_name];
+                return  Params[prmName];
             }
             return "";
         }
-        public int getValue(string prm_name, ref string prm_value)
+        public int GetValue(string prmName, ref string prmValue)
         {
-            if (Params.ContainsKey(prm_name))
+            if (Params.ContainsKey(prmName))
             {
-                prm_name = Params[prm_name];
+                prmName = Params[prmName];
             }
             return 0;
         }
-        public int setValue(string prm_name, string prm_value)
+        public int SetValue(string prmName, string prmValue)
         {
-            if (!Params.ContainsKey(prm_name))
+            if (!Params.ContainsKey(prmName))
             {
-                Params.Add(prm_name, prm_value);
+                Params.Add(prmName, prmValue);
             }
             else
             {
-                Params[prm_name] = prm_value;
+                Params[prmName] = prmValue;
             }
             return 0;
+        }
+
+        public int RunQSheel()
+        {
+            MessageBox.Show("Start QSheel");
+            return 0;
+        }
+        public int WaitTestComplit()
+        {
+            mStatus = CStatus.init;
+            while (CStatus.end_of_test != mStatus)
+            {
+                MessageBox.Show("Try Polling");
+                Thread.Sleep(2000);
+            }
+
+            return 0;
+        }
+
+        public string getStatus()
+        {
+            return convertStatusToString(mStatus);
+        }
+
+        public string convertStatusToString(CStatus xStatus)
+        {
+            switch (xStatus)
+            {
+                case CStatus.busy:
+                    return "BUSY";
+                    break;
+                case CStatus.end_of_test:
+                    return "END_OF_TEST";
+                    break;
+                case CStatus.failed:
+                    return "FAILED";
+                    break;
+                case CStatus.init:
+                    return "INIT";
+                    break;
+                case CStatus.logical_running:
+                    return "LOGICAL_RUNNING";
+                    break;
+                case CStatus.paused:
+                    return "PAUSED";
+                    break;
+                case CStatus.ready:
+                    return "READY";
+                    break;
+                case CStatus.stopped:
+                    return "STOPPED";
+                    break;
+                case CStatus.test_passed:
+                    return "TEST_PASSED";
+                    break;
+                case CStatus.test_failed:
+                    return "TEST_FAILED";
+                    break;
+                case CStatus.retry:
+                    return "RETRY";
+                    break;
+                default:
+                    return "";
+            }
         }
 
     }
--- CSRemoteAgent/CSRAgent.cs
+++ CSRemoteAgent/CSRAgent.cs
@@ -16,15 +16,14 @@ namespace CSRAgent
   [Guid("479DFA08-CF6D-4890-AAAF-7CAFC39B6974"), ComVisible(true), ProgId("AlmCsRemoteAgent1152")]
   public class CSRAgent : ServicedComponent, IRAgent
   {
-    ALMCon m_ALMCon = new ALMCon();
-    private string m_status ;
-    private string m_descr;
+    ALMCon mALMCon = new ALMCon();
+    private string mDescr;
    
 
     public int get_status(ref string descr, ref string status)
     {
-      descr = m_descr;
-      status = m_status;
+      descr = mDescr;
+      status = mALMCon.getStatus();
       return 0;
     }
 
@@ -39,42 +38,40 @@ namespace CSRAgent
     {
       MessageBox.Show(string.Format(
         "Running test: id-{0}, name-{1}, user-{2}, server-{3}, domain-{4}, project-{5}, host-{6}",
-        m_ALMCon.getValue("test_id"),  m_ALMCon.getValue("test_name"),  m_ALMCon.getValue("user_name"), 
-        m_ALMCon.getValue("TDAPI_host_name"),  m_ALMCon.getValue("domain_name"),m_ALMCon.getValue("project_name"),  m_ALMCon.getValue("host_name")));
-      m_ALMCon.OpenCon();
+        mALMCon.GetValue("test_id"),  mALMCon.GetValue("test_name"),  mALMCon.GetValue("user_name"), 
+        mALMCon.GetValue("TDAPI_host_name"),  mALMCon.GetValue("domain_name"),mALMCon.GetValue("project_name"),  mALMCon.GetValue("host_name")));
+      mALMCon.OpenCon();
       List LTestData = new List();
-      LTestData = m_ALMCon.conn.get_Fields(m_ALMCon.getValue("test_set_id"));
+      LTestData = mALMCon.conn.get_Fields(mALMCon.GetValue("test_set_id"));
       // post the run
-      PostRun("Passed");
+      mALMCon.RunQSheel();
+      mALMCon.WaitTestComplit();
+      PostRun(mALMCon.getStatus());//"Passed");
       //Process.GetCurrentProcess().Kill();
-      m_status = "END_OF_TEST";
-      m_descr = "Completed";
+      mALMCon.mStatus = CStatus.end_of_test; //"END_OF_TEST";
+      mDescr = "Completed";
       return 0;
     }
 
     private void PostRun(string runStatus)
     {
-      /*TDConnection conn = new TDConnectionClass();
-      conn.InitConnectionEx( m_ALMCon.getValue("TDAPI_host_name"));
-      conn.ConnectProjectEx( m_ALMCon.getValue("domain_name"),  m_ALMCon.getValue("project_name"),  m_ALMCon.getValue("user_name"),  m_ALMCon.getValue("password"));
-*/
-      m_ALMCon.OpenCon();
+      mALMCon.OpenCon();
 
-      Run run = (((((((m_ALMCon.conn.TestSetFactory as TestSetFactory)[ m_ALMCon.getValue("test_set_id")]) as TestSet).TSTestFactory as TSTestFactory)[ m_ALMCon.getValue("testcycle_id_integer")]) as TSTest).RunFactory as RunFactory).AddItem("scottyRun") as Run;
-      run["RN_TESTER_NAME"] =  m_ALMCon.getValue("user_name");
-      run["RN_HOST"] =  m_ALMCon.getValue("host_name");
+      Run run = (((((((mALMCon.conn.TestSetFactory as TestSetFactory)[ mALMCon.GetValue("test_set_id")]) as TestSet).TSTestFactory as TSTestFactory)[ mALMCon.GetValue("testcycle_id_integer")]) as TSTest).RunFactory as RunFactory).AddItem("scottyRun") as Run;
+      run["RN_TESTER_NAME"] =  mALMCon.GetValue("user_name");
+      run["RN_HOST"] =  mALMCon.GetValue("host_name");
       run.Status = runStatus;
       run.Post();
     }
 
-    public int get_value(string prm_name, ref string prm_value)
+    public int get_value(string prmName, ref string prmValue)
     {
-        return m_ALMCon.getValue(prm_name,ref prm_value);
+        return mALMCon.GetValue(prmName,ref prmValue);
     }
 
-    public int set_value(string prm_name, string prm_value)
+    public int set_value(string prmName, string prmValue)
     {
-      return m_ALMCon.setValue(prm_name, prm_value);
+      return mALMCon.SetValue(prmName, prmValue);
     }
 
     public int stop()
