2107f7237a5de6f4acf1177bbbeb34dbd95e930b
abugov <abugov@gmail.com>
2017-03-09 12:42:01 +0200

    MAJOR AGENT CLEANUP

---
+++ CSRemoteAgent/AgentRunManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using QS.ALM.CloudShellApi;
+
+namespace CSRAgent
+{
+    class AgentRunManager
+    {
+        public int RunTest(Api api, string testPath)
+        {
+            string contentError;
+            bool isSuccess;
+            string guiId = api.RunTest(testPath, null, out contentError, out isSuccess);
+
+            if (isSuccess)
+            {
+                ExecutionJobResult runResult;
+
+                using (var runStatusManager = new RunStatusManager(guiId, api))
+                    runResult = runStatusManager.WaitForRunEnd();
+            }
+            else
+            {
+                throw new Exception(contentError);
+            }
+
+            return 0;
+        }
+
+        /*public string getStatus()
+        {
+            return convertStatusToString(mStatus);
+        }
+
+        private string convertStatusToString(CStatus xStatus)
+        {
+            switch (xStatus)
+            {
+                case CStatus.busy:
+                    return "BUSY";
+                case CStatus.end_of_test:
+                    return "END_OF_TEST";
+                case CStatus.failed:
+                    return "FAILED";
+                case CStatus.init:
+                    return "INIT";
+                case CStatus.logical_running:
+                    return "LOGICAL_RUNNING";
+                case CStatus.paused:
+                    return "PAUSED";
+                case CStatus.ready:
+                    return "READY";
+                case CStatus.stopped:
+                    return "STOPPED";
+                case CStatus.test_passed:
+                    return "TEST_PASSED";
+                case CStatus.test_failed:
+                    return "TEST_FAILED";
+                case CStatus.retry:
+                    return "RETRY";
+                default:
+                    return "";
+            }
+        }*/
+    }
+}
---
+++ CSRemoteAgent/AlmTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+using TDAPIOLELib;
+
+namespace CSRAgent
+{
+    class AlmTest
+    {
+        public string GetTestPath(TSTest test)
+        {
+            var theTest = (Test)test.Test;
+            if (theTest["TS_USER_01"] != null)
+                return theTest["TS_USER_01"].ToString();
+            
+            throw new Exception("Test path not selected");
+        }
+
+        public TSTest FindTest(AlmConnection almConnection, AlmParameters almParameters)
+        {
+            string[] strParmsArr;
+            string testF = "";
+            string testn = "";
+            //int TestInstID = Convert.ToInt32(almParameters.Params["testcycle_id_integer"]);
+            if (almParameters.TestSet.Split(',')[0].Split('\"')[0].IndexOfAny("ntest_set".ToCharArray()) > -1)
+            {
+                strParmsArr = almParameters.TestSet.Split(',')[0].Split('\"')[1].Split('\\');
+                testF = strParmsArr[2];
+                testn = strParmsArr[strParmsArr.Count() - 1];
+
+            }
+            else
+            {
+                throw new Exception("ERROR 99"); //TODO
+            }
+
+            //TreeManager treeMgr = (TreeManager)almConnection.Connection.TreeManager;
+            //SubjectNode subjectNode = (SubjectNode)treeMgr.get_NodeByPath("Subject\\" + testF);
+            TestSetTreeManager testSetFolderF = (TestSetTreeManager)almConnection.Connection.TestSetTreeManager;
+            TestSetFolder tstSetFolder = (TestSetFolder)testSetFolderF.NodeByPath["Root"];
+
+            var theTestSet = FindTestSet(tstSetFolder, testn);
+            string testName = almParameters.TestName;
+            var TSTestFact = (TSTestFactory)theTestSet.TSTestFactory;
+            var tsFilter = (TDFilter)TSTestFact.Filter;
+            tsFilter["TC_CYCLE_ID"] = theTestSet.ID.ToString();
+            var testList = TSTestFact.NewList(tsFilter.Text);
+
+            foreach (TSTest TSTst in testList)
+            {
+                if (testName == TSTst.TestName)
+                    return TSTst;;
+            }
+
+            throw new Exception("ERROR 98"); //TODO
+        }
+
+        private static TestSet FindTestSet(TestSetFolder testSetF, string tsName)
+        {
+            TestSet testSet1 = null;
+
+            var tsList = testSetF.FindTestSets(tsName);
+            if (tsList != null)
+            {
+                if (tsList.Count > 0)
+                {
+                    foreach (TestSet ts in tsList)
+                    {
+                        if (ts.Name == tsName)
+                        {
+                            testSet1 = ts;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (testSet1 == null)
+                throw new Exception("testSet not found");
+
+            return testSet1;
+        }
+
+        public object GetTestParameters(TSTest tsTst)
+        {
+            var supportParameterValues = (ISupportParameterValues)tsTst;
+            var testParametersVList = supportParameterValues.ParameterValueFactory.NewList("");
+
+            if (testParametersVList != null)
+            {
+                foreach (ParameterValue element in testParametersVList)
+                {
+                    string value = GetParameterValue(element.ActualValue);
+                    string Ls = element.Name;
+                }
+            }
+
+            return null;
+        }
+
+        private string GetParameterValue(object p)
+        {
+            return "";
+        }
+    }
+}
