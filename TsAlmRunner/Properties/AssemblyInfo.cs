e44513a1dfe0e5d34c27bbe31e43cde8791702c4
abugov <abugov@gmail.com>
2017-03-17 15:29:00 +0200

    AssemblyInfo cleanup

--- TsAlmRunner/Properties/AssemblyInfo.cs
+++ TsAlmRunner/Properties/AssemblyInfo.cs
@@ -1,10 +1,9 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("TestShellAgent")]
-[assembly: AssemblyDescription("ALM TestShell Remote Agent")]
+[assembly: AssemblyTitle("TsAlmRunner")]
+[assembly: AssemblyProduct("TsAlmRunner")]
 [assembly: AssemblyCompany("Quali")]
-[assembly: AssemblyProduct("ALM TestShell Remote Agent")]
 [assembly: AssemblyCopyright("Copyright © Quali 2017")]
 [assembly: ComVisible(false)]
 [assembly: Guid("35c00763-ce7d-4a7c-91d0-4a02ba593bd6")]
--- TsCloudShellApi/Properties/AssemblyInfo.cs
+++ TsCloudShellApi/Properties/AssemblyInfo.cs
@@ -1,36 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("QS.ALM.CloudShellApi")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("QS.ALM.CloudShellApi")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyTitle("TsCloudShellApi")]
+[assembly: AssemblyProduct("TsCloudShellApi")]
+[assembly: AssemblyCompany("Quali")]
+[assembly: AssemblyCopyright("Copyright © Quali 2017")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a6a56539-dc75-42ff-8008-da147b56d93c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
