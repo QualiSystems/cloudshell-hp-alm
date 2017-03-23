ec7a1ef6a625157989bd26941cd915912de3ace4
AndreyMHP <andrey.m@ticomsoft.com>
2017-03-23 18:49:38 +0200

    Adding System.Windows.Forms.TreeView and icons

--- TsTestType/Tree/AlmTreeNode.cs
+++ TsTestType/Tree/AlmTreeNode.cs
@@ -4,7 +4,18 @@ namespace TsTestType.Tree
 {
     public class AlmTreeNode : ITreeNode
     {
+
+        private static readonly System.Windows.Forms.ImageList m_ImageList;
         public UltraTreeNode Node { get; private set; }
+
+        static AlmTreeNode()
+        {
+            m_ImageList = new System.Windows.Forms.ImageList();
+            m_ImageList.Images.Add(Resource.Local);
+            m_ImageList.Images.Add(Resource.Shared);
+            m_ImageList.Images.Add(Resource.Folder);
+            m_ImageList.Images.Add(Resource.Test);
+        }
 
         public AlmTreeNode(UltraTreeNode node)
         {
@@ -27,5 +38,7 @@ namespace TsTestType.Tree
             get { return Node.Selected; }
             set { Node.Selected = value; }
         }
+
+        public int ImageIndex { get { return 0; } set { Node.LeftImages.Add(m_ImageList.Images[value]); } }
     }
 }
